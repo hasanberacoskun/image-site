2418bc9f04befe88bfc59fc1f0d517ba44decf20
Bera Coskun <hasanberacoskun@gmail.com>
2020-12-16 03:36:07 -0800

    Made posts viewable.

--- application/routes/index.js
+++ application/routes/index.js
@@ -2,6 +2,7 @@ var express = require('express');
 var router = express.Router();
 var isLoggedIn = require('../middleware/routeProtectors').userIsLoggedIn;
 var getRecentPosts = require('../middleware/postsmiddleware').getRecentPosts;
+var db = require("../config/database");
 
 /* GET home page. */
 router.get('/', getRecentPosts, function(req, res, next) {
@@ -34,4 +35,19 @@ router.get('/postimage', (req, res, next) => {
   });
 });
 
+router.get('/post/:id(\\d+)', (req, res, next) => {
+  let baseSQL = "SELECT u.id, u.username, p.title, p.description, p.photopath, p.created FROM users u JOIN posts p ON u.id=fk_userid WHERE p.id=?;";
+  let postId = req.params.id;
+  db.execute(baseSQL, [postId])
+  .then(([results, fields]) => {
+    if(results && results.length) {
+      let post = results[0];
+      res.render('imagepost', {currentPost: post, style: "imagepost.css"});
+    } else {
+      req.flash('error', "This post does not exist.");
+      res.redirect('/');
+    }
+  })
+
+});
 module.exports = router;
