68549d4d67bfebdb232d76d074dd74f60a1a63f4
Bera Coskun <hasanberacoskun@gmail.com>
2024-11-03 19:54:33 -0800

    Updated readme to include screenshots and made dotenv changes to follow better practices.

--- application/config/database.js
+++ application/config/database.js
@@ -1,13 +1,13 @@
-const mysql = require('mysql2');
+const mysql = require("mysql2");
 
 const pool = mysql.createPool({
-  host: 'localhost',
-  user: 'photoapp',
-  password: 'password',
-  database: 'csc317db',
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DATABASE,
   connectionLimit: 50,
   debug: false,
-  port: 3306
+  port: process.env.DB_PORT,
 });
 
 const promisePool = pool.promise();
